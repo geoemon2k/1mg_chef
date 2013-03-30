927dd8b46454aec6edb45837ae8e766f24eb05ee
Keiji <Keiji@Keiji-PC>
2013-03-30 15:31:41 +0900

    fixed features

--- features/1mg/features_detail/step_definitions/sendmail_step.rb
+++ features/1mg/features_detail/step_definitions/sendmail_step.rb
@@ -1,17 +1,27 @@
 # encoding: UTF-8
 
-前提(/^: ssh で グローバルIPアドレス の (\d+) 番ポートへ 一般 ユーザで接続する$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+前提(/^: (\w+) の (\d+) 番ポートから外部へメールを送信ができる$/) do |hostname, port|
+  require 'mail'
+
+  mail = Mail.new do
+  from    'test'
+  to      'geoemon2kkk@yahoo.co.jp'
+  subject 'This is Cucumber Test'
+  body    'This is Cucumvber Test.'
+
+  mail.delivery_method :smtp, { address:   #{hostname},
+                              port:      #{port},
+                              domain:    '1mg.org',
+                              user_name: 'root',
+                              password:  ($stderr.print 'password> '; gets.chomp) }
+  mail.deliver!
 end
 
-ならば(/^: 成功する$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-前提(/^: ssh で グローバルIPアドレス の (\d+) 番ポートへ root ユーザで接続する$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-ならば(/^: 失敗する$/) do
-  pending # express the regexp above with the code you wish you had
+前提(/^: グローバルIPの (\d+) 番ポートへの アクセスすると (\w+) が返る $/) do |port, return|
+  @gip = `ip -f inet addr show eth0`
+  @ip =~ /\d+\d+\d+\d+/
+  p @ip 
+  require 'socket'
+  s = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+  sockaddr = Socket.sockaddr_in(#{port}, @gip)
 end
