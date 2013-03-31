4a5c1b20037b2a5be03204b153c419fe80d5bd8a
Keiji <Keiji@Keiji-PC>
2013-03-31 10:47:13 +0900

    fixed dns_step.rb

--- features/1mg/features/dns.feature
+++ features/1mg/features/dns.feature
@@ -4,13 +4,13 @@
 機能: 権威DNS
 
 シナリオ: 権威DNS機能確認(1mg.org)
-もし: グローバルIPアドレスへ 1mg.org の SOA レコードを問い合わせる
+もし: 権威DNS グローバルIPアドレスへ 1mg.org の SOA レコードを問い合わせる
 ならば: status が NOERROR で SOA レコードにシリアルが含まれている
 
 シナリオ: 権威DNS機能確認(1mg.biz)
-もし: グローバルIPアドレスへ 1mg.biz の SOA レコードを問い合わせる
+もし: 権威DNS グローバルIPアドレスへ 1mg.biz の SOA レコードを問い合わせる
 ならば: status が NOERROR で SOA レコードにシリアルが含まれている
 
 シナリオ: キャッシュDNS機能OFF確認
-もし: グローバルIPアドレスへ yahoo.co.jp の SOA レコードを問い合わせる
+もし: キャッシュDNSグローバルIPアドレスへ yahoo.co.jp の SOA レコードを問い合わせる
 ならば: statusが REFUSED である
--- features/1mg/features/step_definitions/dns_step.rb
+++ features/1mg/features/step_definitions/dns_step.rb
@@ -9,18 +9,18 @@ for ip in @ips do
   end
 end
   
-もし(/^: グローバルIPアドレスへ ([\w\.]+) の SOA レコードを問い合わせる$/) do |domain|
+もし(/^: 権威DNS グローバルIPアドレスへ ([\w\.]+) の SOA レコードを問い合わせる$/) do |domain|
   @response = `dig @#{$gip} #{domain} SOA|egrep 'status:|^#{domain}'`
 end
 
 ならば(/^: status が NOERROR で SOA レコードにシリアルが含まれている$/) do
     @response.should match(/\:\s+NOERROR\,/)
-    @response.should match(/[\s\t]+IN[\s\t]+SOA[\s\t]+#{domain}[\s\t]+[\w\.\-]+\.[\s\t]+[\w\.\-]+\.[\s\t]+\d+\s+/)
-p @response
+    @response.should match(/[\s\t]+IN[\s\t]+SOA[\s\t]+[\w\.\-]+\.[\s\t]+[\w\.\-]+\.[\s\t]+\d+\s+/)
 end
 
-もし(/^: グローバルIPアドレスへ yahoo.co.jp の SOA レコードを問い合わせる$/) do
-  @response = `dig @#{$gip} yahoo.co.jp SOA|egrep 'status:'`
+もし(/^: キャッシュDNS グローバルIPアドレスへ yahoo.co.jp の SOA レコードを問い合わせる$/) do
+  @response = `dig @#{$ip} yahoo.co.jp SOA|egrep 'status:'`
+p @response
 end
 
 ならば(/^: statusが REFUSED である$/) do
