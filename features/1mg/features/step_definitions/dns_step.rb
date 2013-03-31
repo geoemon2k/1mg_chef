4489766757cced059d251082a9bc02f878bb4724
Keiji <Keiji@Keiji-PC>
2013-03-31 10:35:22 +0900

    add dns_step.rb

---
+++ features/1mg/features/step_definitions/dns_step.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+
+@ips = `ip -f inet addr show`
+@ips = @ips.scan(/\d+\.\d+\.\d+\.\d+/)
+for ip in @ips do
+  if ip != '127.0.0.1'
+    $gip = ip
+    break
+  end
+end
+  
+もし(/^: グローバルIPアドレスへ ([\w\.]+) の SOA レコードを問い合わせる$/) do |domain|
+  @response = `dig @#{$gip} #{domain} SOA|egrep 'status:|^#{domain}'`
+end
+
+ならば(/^: ならば: status が NOERROR で SOA レコードにシリアルが含まれている$/) do
+    @response.should match(/\:\s+NOERROR\,/)
+    @response.should match(/[\s\t]+IN[\s\t]+SOA[\s\t]+[\w\.\-]+\.[\s\t]+[\w\.\-]+\.[\s\t]+\d+\s+/)
+p @response
+end
+
+もし(/^: グローバルIPアドレスへ yahoo.co.jp の SOA レコードを問い合わせる$/) do
+  @response = `dig @#{$ip} yahoo.co.jp SOA|egrep 'status:'`
+end
+
+ならば(/^: statusが REFUSED である$/) do
+    @response.should match(/\:\s+REFUSED\,/)
+end
