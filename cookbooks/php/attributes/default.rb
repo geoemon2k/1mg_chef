85da2ffebfe0b7307f2c3e9133e267b24c0f0638
uehata <keiji.ue@gmail.com>
2013-04-03 16:24:38 +0900

    fixed php/attributes

--- cookbooks/php/attributes/default.rb
+++ cookbooks/php/attributes/default.rb
@@ -3,8 +3,4 @@ node.default['php']['packages'] = {
   "php-mysql" => "",
   "php-mbstring" => "",
   "php-mcrypt" => "--enablerepo=epel",
-  "php-pear" => "",
-  "php-devel" => "",
-  "pcre" => "",
-  "pcre-devel" => ""
 }
