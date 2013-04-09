f6808c81b72567ceedc05642e98604c41392769b
uehata <keiji.ue@gmail.com>
2013-04-09 22:10:22 +0900

    fixed recipes/nsd

--- cookbooks/nsd/attributes/default.rb
+++ cookbooks/nsd/attributes/default.rb
@@ -1,4 +1,4 @@
-node.default['zones']['list'] = {
+default['zone']['lists'] = {
 '1mg.org' => {
   'zonefile' => '"zones/1mg.org.zone"',
   'xfr' => ['127.0.0.1','123.50.202.226','198.104.58.211']},
--- cookbooks/nsd/recipes/default.rb
+++ cookbooks/nsd/recipes/default.rb
@@ -1,31 +1,54 @@
-package 'nsd' do
-  action :install
-  options '--enablerepo=epel'
+options = nil
+case node[:platform]
+when "centos"
+  pkg_name = 'nsd'
+  etc_path = '/etc/nsd/'
+  options = '--enablerepo=epel'
+when "ubuntu"
+  pkg_name = 'nsd3'
+  etc_path = '/etc/nsd3/'
 end
 
-cookbook_file '/etc/nsd/nsd.conf' do
+package "#{pkg_name}" do
+  action :install
+
+  if "#{options}" != nil
+    options "#{options}"
+  end
+end
+
+cookbook_file "#{etc_path}nsd.conf" do
   source 'nsd.conf'
   notifies :restart, 'service[nsd]'
 end
 
-template '/etc/nsd/other.conf' do
+template "#{etc_path}other.conf' do
   source 'other.conf.erb'
   owner 'root'
   group 'root'
   mode '0644'
-  variables({
-    :zones => node[:zones]
-  })
   notifies :restart, 'service[nsd]'
 end
 
-directory "/etc/nsd/zones" do
+directory "#{etc_path}zones" do
   action :create
   owner 'nsd'
   group 'nsd'
   mode '0755'
 end
 
+node.zone.lists.each_pair do |zone_name, value|
+  cookbook_file "#{etc_path}zones/#{zone_name}.zone" do
+    source "zones/#{zone_name}.zone"
+    owner 'nsd'
+    group 'nsd'
+    mode '0644'
+    notifies :rebuild, 'service[nsd]'
+    notifies :reload, 'service[nsd]'
+  end
+end
+
 service 'nsd' do
   action [:enable, :start]
+  supports :reload => true, :status => true, :restart => true, :rebuild => true
 end
