79b361f4dca98ca7af2a9edd29f1b12953ef8610
uehata <keiji.ue@gmail.com>
2013-04-09 21:18:02 +0900

    delete recipes/{remi,epel}

--- cookbooks/remi/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-node.default['remi']['gpgkey'] = "RPM-GPG-KEY-remi"
-node.default['remi']["gpg"] = "http://rpms.famillecollet.com/#{node['remi']['gpgkey']}"
-node.default['remi']["rpm"] = "http://rpms.famillecollet.com/enterprise/remi-release-#{node['platform_version'].to_i}.rpm"
