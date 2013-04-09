71062af1fe3b451f2e60a1b7282f3f742c7dc665
Super-User <root@1bbbbf67-6f43-4ace-b942-478922d0e208.(none)>
2013-04-09 21:05:01 +0900

    add recipe yum/epel

---
+++ cookbooks/yum/attributes/epel.rb
@@ -0,0 +1,1 @@
+default['yum']['epel']['disabled'] = false
---
+++ cookbooks/yum/recipes/epel.rb
@@ -0,0 +1,28 @@
+gpg_keyname = "RPM-GPG-KEY-EPEL-#{node[:platform_version].to_i}"
+if node['platform_version'].to_i > 5
+  url = "http://ftp.iij.ad.jp/pub/linux/fedora/epel/#{node['platform_version'].to_i}/#{node['kernel']['machine']}/epel-release-6-8.noarch.rpm"
+else
+  url = "http://ftp.iij.ad.jp/pub/linux/fedora/epel/#{node['platform_version'].to_i}/#{node['kernel']['machine']}/epel-release-5-4.noarch.rpm"
+end
+
+
+execute "install_epel_gpg-key" do
+  action :run
+  command "rpm --import http://ftp.iij.ad.jp/pub/linux/fedora/epel/#{gpg_keyname}"
+  not_if "ls /etc/pki/rpm-gpg/#{gpg_keyname}"
+end
+
+execute "install_epel_repo" do
+  action :run
+  command "rpm --install #{url}"
+  not_if 'rpm -q epel-release'
+
+end
+
+if node['yum']['epel']['disabled']
+  execute "disable_epel_repo" do
+    action :run
+    command "sed -i -e 's/enabled=1/enabled=0/g' /etc/yum.repos.d/epel.repo"
+    only_if 'grep "enabled=1" /etc/yum.repos.d/epel.repo'
+  end
+end
