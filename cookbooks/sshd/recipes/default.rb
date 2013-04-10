817baafaec8ba89875f051edf850c0364fa5046b
uehata <keiji.ue@gmail.com>
2013-04-10 20:58:40 +0900

    fixed sshd/recipes

--- cookbooks/sshd/recipes/default.rb
+++ cookbooks/sshd/recipes/default.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: sshd
-# Recipe:: default
+# Recipe:: node
 #
 # Copyright 2013, YOUR_COMPANY_NAME
 #
@@ -10,22 +10,22 @@ package node['sshd']['package'] do
   action :install
 end
 
-if node.sshd.conf_lists
-  node.sshd.conf_lists.each_pair do |name, value|
+if node['sshd']['conf_lists']
+  node['sshd']['conf_lists'].each_pair do |name, value|
     execute "replace_set_sshd_config_#{name}" do
-      command "sed -i -e \"s/^#{name} \.*/#{name} #{value}/g\" #{default['sshd']['dir']}sshd_config"
-      not_if "egrep '^#{name} #{value}' #{default['sshd']['dir']}sshd_config"
-      notifies :restart, "service[#{default['sshd']['service_name']}]"
+      command "sed -i -e \"s/^#{name} \.*/#{name} #{value}/g\" #{node['sshd']['dir']}sshd_config"
+      not_if "egrep '^#{name} #{value}' #{node['sshd']['dir']}sshd_config"
+      notifies :restart, "service[#{node['sshd']['service_name']}]"
     end
     execute "add_sshd_config_#{name}" do
-      command "echo '#{name} #{value}' >> #{default['sshd']['dir']}sshd_config"
-      not_if "egrep '^#{name} #{value}' #{default['sshd']['dir']}sshd_config"
-      notifies :restart, "service[#{default['sshd']['service_name']}]"
+      command "echo '#{name} #{value}' >> #{node['sshd']['dir']}sshd_config"
+      not_if "egrep '^#{name} #{value}' #{node['sshd']['dir']}sshd_config"
+      notifies :restart, "service[#{node['sshd']['service_name']}]"
     end
   end
 end
 
-service default['sshd']['service_name'] do
+service node['sshd']['service_name'] do
   action [:enable, :start]
   supports :status => true, :restart => true
 end
