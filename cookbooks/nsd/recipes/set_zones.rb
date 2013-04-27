17df4ec6bb283e11bf64d93561bc9f28faa72e9b
Keiji <Keiji@Keiji-PC>
2013-04-27 14:42:58 +0900

    fixed recipe/nsd

--- cookbooks/nsd/recipes/set_zones.rb
+++ cookbooks/nsd/recipes/set_zones.rb
@@ -1,6 +1,6 @@
-node.zone_list.each_pair do |zone_name, value|
-  cookbook_file "/etc/nsd/zones/#{zone_name}.zone" do
-    source "zones/#{zone_name}.zone"
+node['nsd']['zone_list'].each_pair do |zone_name, value|
+  cookbook_file "/etc/nsd/zones/"+zone_name+".zone" do
+    source "zones/"+zone_name+".zone"
     owner 'nsd'
     group 'nsd'
     mode '0644'
