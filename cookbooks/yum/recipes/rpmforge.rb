52180e35a7a16d641e5c18ebbadd522acb6dbad5
uehata <keiji.ue@gmail.com>
2013-05-14 17:39:28 +0900

    fixed rpmforge

--- cookbooks/yum/recipes/rpmforge.rb
+++ cookbooks/yum/recipes/rpmforge.rb
@@ -1,20 +1,20 @@
 execute "install-rpmforge-key" do
   action :run
-  command "rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt"
-  not_if 'ls /etc/pki/rpm-gpg/RPM-GPG-KEY.dag'
+  command "rpm --import #{node['yum']['rpmforge']['gpg_url']}"
+  not_if 'ls /etc/pki/rpm-gpg/#{node['yum']['rpmforge']['gpg']}'
 end
 
 execute "install-rpmforge" do
   action :run
-  command "rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-#{node['platform_version'].to_i}.rpm"
-  not_if "rpm -q remi-release"
+  command "rpm -ivh #{node['yum']['rpmforge']['url']}"
+  not_if "rpm -q rpmforge-release"
 end
 
-if node['yum']['remi']['disabled']
-  execute "disable-remi" do
+if node['yum']['rpmforge']['disabled']
+  execute "disable-rpmforge" do
     action :run
-    command "sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/remi"
-    only_if "grep 'enabled=1' /etc/yum.repos.d/remi"
+    command "sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/rpmforge"
+    only_if "grep 'enabled=1' /etc/yum.repos.d/rpmforge"
   end
 else
   execute "enable-rpmforge" do
