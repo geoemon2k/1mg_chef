d2bdeb13d566b894bc329e1aa9d01d6838f4435f
Keiji.ue <keiji.ue@gmail.com>
2013-12-13 00:18:56 +0900

    test

--- cookbooks/1mg/recipes/stop_service.rb
+++
@@ -1,7 +0,0 @@
-if node['1mg']['stop_services']
-  node['1mg']['stop_services'].each do |service_name|
-    service service_name do
-      action [:disable, :stop]
-    end
-  end
-end
---
+++ cookbooks/base/recipes/stop_service.rb
@@ -0,0 +1,9 @@
+if node['base']['stop_services']
+  node['base']['stop_services'].each do |service_name|
+    service service_name do
+      action [:disable, :stop]
+    end
+  end
+end
+
+#test
