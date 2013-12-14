679f76fa5f47a61b920d73e55603ef6ca52a223a
uehata <uehata@firstserver.co.jp>
2013-12-15 01:24:30 +0900

    fixed spawn

--- cookbooks/spawn-fcgi/attributes/default.rb
+++ cookbooks/spawn-fcgi/attributes/default.rb
@@ -4,6 +4,7 @@ default['spawnfcgi']['group'] = 'apache'
 default['spawnfcgi']['num'] = '32'
 default['spawnfcgi']['pid'] = '/var/run/spawn-fcgi.pid'
 default['spawnfcgi']['pkg_name'] = 'spawn-fcgi'
+default['spawnfcgi']['service'] = 'spawn-fcgi'
 case node[:platform]
 when 'centos'
   default['spawnfcgi']['pkg_options'] = '--enablerepo=epel'
