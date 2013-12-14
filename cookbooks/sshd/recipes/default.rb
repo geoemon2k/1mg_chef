4655375994678b6820ae876381b90734423e09ed
uehata <uehata@firstserver.co.jp>
2013-12-14 16:01:53 +0900

    fixed base

--- cookbooks/sshd/recipes/default.rb
+++ cookbooks/sshd/recipes/default.rb
@@ -16,9 +16,9 @@ if node['sshd']['conf_lists'] != nil
       block do
         _fe = Chef::Util::FileEdit.new(node['sshd']['sshd_config'])
         # 追加
-        _fe.insert_line_if_no_match('^' + name + " " + value, name + " " + value)
+        _fe.insert_line_if_no_match('^' + name + "(\t|\s)+.*", name + " " + value)
         # 置き換え
-        _fe.search_file_replace('^' + name + "(\t|\s)+" + value, name + " " + value)
+        _fe.search_file_replace('^' + name + "(\t|\s)+.*", name + " " + value)
         _fe.write_file
       end
       notifies :restart, "service[" + node['sshd']['service_name'] + "]"
