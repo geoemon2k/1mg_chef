441af8752fd577e0e961084f58977c5775240c0b
uehata <uehata@firstserver.co.jp>
2014-01-03 13:02:27 +0900

    add jenkins

---
+++ cookbooks/jenkins/recipes/setup.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: jenkins
+# Recipe:: setup
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "java-1.6.0-openjdk" do
+  action :install
+end
+
+template node['base']['etc'] + '/yum.repos.d/jenkins.repo' do
+  source "jenkins.repo.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :restart, "service[jenkins]"
+end
+
+package "jenkins" do
+  action :install
+  options '--enablerepo=jenkins'
+end
+
+service "jenkins" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
+#template node['base']['etc'] + '/sysconfig/jenkins' do
+#  source "sysconfig_jenkins.erb"
+#  owner "root"
+#  group "root"
+#  mode "0644"
+#end
+
