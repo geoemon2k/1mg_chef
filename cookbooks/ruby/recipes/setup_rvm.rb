c91d3c1c3dcff2621d8377129c48ef1b116e23cb
uehata <uehata@firstserver.co.jp>
2014-01-05 11:06:14 +0900

    fixed rvm

--- cookbooks/ruby/recipes/setup_rvm.rb
+++ cookbooks/ruby/recipes/setup_rvm.rb
@@ -1,15 +1,18 @@
-%['curl' 'curl-devel' 'gcc' 'gcc-c++' 'git' 'openssl-devel' 'httpd-devel' 'readline-devel' 'tk-devel' 'make' 'zlib-devel' 'libffi-devel'].each do |pkg|
+%{'curl' 'curl-devel' 'gcc' 'gcc-c++' 'git' 'openssl-devel' 'httpd-devel' 'readline-devel' 'tk-devel' 'make' 'zlib-devel' 'libffi-devel'}.each do |pkg|
   package pkg do
     action :install
+    not_if "which rvm"
   end
 end
 
 package 'libyaml-devel' do
   options '--enablerepo=epel'
   action :install
+  not_if "which rvm"
 end
 
 execute 'setup_rvm' do
   command "bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm ... -installer )"
   action run
+  not_if "which rvm"
 end
