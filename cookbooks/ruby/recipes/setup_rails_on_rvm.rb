c54366ef66f5d4f40055e2b1c371130f20135422
uehata <uehata@firstserver.co.jp>
2014-01-05 12:54:10 +0900

    fixed rails

--- cookbooks/ruby/recipes/setup_rails_on_rvm.rb
+++ cookbooks/ruby/recipes/setup_rails_on_rvm.rb
@@ -7,15 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 execute "create_gemset_rails" do
-  command "rvm gemset create rails-" + node['ruby']['rails_version']
+  command "/usr/local/rvm/bin/rvm gemset create rails-" + node['ruby']['rails_version']
   action :run
-  not_if "rvm gemset list|grep 'rails-" + node['ruby']['rails_version'] + "'"
+  not_if "/usr/local/rvm/bin/rvm gemset list|grep 'rails-" + node['ruby']['rails_version'] + "'"
 end
 
 execute "change_gemset_rails" do
-  command "rvm " + node['ruby']['version'] + '@rails-' + node['ruby']['rails_version']
+  command "/usr/local/rvm/bin/rvm " + node['ruby']['version'] + '@rails-' + node['ruby']['rails_version']
   action :run
-  not_if "rvm gemset list|grep =>|grep 'rails-" + node['ruby']['rails_version'] + "'" 
+  not_if "/usr/local/rvm/bin/rvm gemset list|grep =>|grep 'rails-" + node['ruby']['rails_version'] + "'" 
 end
 
 gem_package "rails" do
