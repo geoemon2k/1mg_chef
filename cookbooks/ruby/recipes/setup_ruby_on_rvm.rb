98a6fd042cbda07eaef00670286267a46b1b3ce2
uehata <uehata@firstserver.co.jp>
2014-01-05 11:36:23 +0900

    fixed ruby

--- cookbooks/ruby/recipes/setup_ruby_on_rvm.rb
+++ cookbooks/ruby/recipes/setup_ruby_on_rvm.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 execute "setup_ruby" do
-  command "rvm install " + node['ruby']['version']
-  not_if "rvm list|egrep " + node['ruby']['version']
+  command "`which rvm` install " + node['ruby']['version']
+  not_if "`which rvm` list|egrep " + node['ruby']['version']
   action :run
 end
