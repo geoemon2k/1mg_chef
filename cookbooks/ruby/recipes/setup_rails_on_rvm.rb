8d4991b358f83ed71731e96c6df1bd5a111116d3
uehata <uehata@firstserver.co.jp>
2014-01-05 12:45:58 +0900

    add rails

---
+++ cookbooks/ruby/recipes/setup_rails_on_rvm.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: ruby
+# Recipe:: setup_rails_on_rvm
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+execute "create_gemset_rails" do
+  command "rvm gemset create rails-" + node['ruby']['rails_version']
+  action :run
+  not_if "rvm gemset list|grep 'rails-" + node['ruby']['rails_version'] + "'"
+end
+
+execute "change_gemset_rails" do
+  command "rvm " + node['ruby']['version'] + '@rails-' + node['ruby']['rails_version']
+  action :run
+  not_if "rvm gemset list|grep =>|grep 'rails-" + node['ruby']['rails_version'] + "'" 
+end
+
+gem_package "rails" do
+  action :install
+end
