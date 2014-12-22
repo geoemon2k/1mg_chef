69bbcf580bdb7f4b6942d5328fe410bcd346a7e7
uehata_new <uehata2@firstserver.co.jp>
2014-12-22 10:30:11 +0900

    add vsftpd

--- cookbooks/iptables/attributes/iptables.rb
+++ cookbooks/iptables/attributes/iptables.rb
@@ -1,9 +1,11 @@
 include_attribute "base::default"
 
-node.default['iptables']['port'] = {
+node.default['iptables']['port']['tcp'] = {
   '22' => ['0.0.0.0/0'],
   '80' => ['0.0.0.0/0'],
-  '53' => ['0.0.0.0/0']
+  '53' => ['0.0.0.0/0'],
+  '21' => ['0.0.0.0/0'],
+  '20' => ['0.0.0.0/0']
 }
 
 case node[:platform]
---
+++ cookbooks/vsftpd/recipes/setup.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: vsftpd
+# Recipe:: setup
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package node['vsftpd']['pkg_name'] do
+  action :install
+  if node['vsftpd']['pkg_options']
+    options node['vsftpd']['pkg_options']
+  end
+end
+
+#template node['vsftpd']['etc'] + "/vsftpd.conf" do
+#  source "vsftpd.conf.erb"
+#  owner node['vsftpd']['user']
+#  group node['vsftpd']['group']
+#  mode  0644
+#  notifies :reload, 'service[' + node['vsftpd']['service'] + ']'
+#end
+
+service node['vsftpd']['service'] do
+  action [:enable, :start]
+  supports :status => true, :restart => true
+end
