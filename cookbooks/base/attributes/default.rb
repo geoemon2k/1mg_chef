408c914a98722700982c72c652c34d984289128c
Keiji.ue <keiji.ue@gmail.com>
2015-02-15 15:58:17 +0900

    add backup.sh

--- cookbooks/base/attributes/default.rb
+++ cookbooks/base/attributes/default.rb
@@ -2,7 +2,7 @@ default['base']['stop_services'] = Array.new
 default['base']['root_address'] = 'root@localhost'
 default['base']['etc'] = '/etc'
 default['base']['hostname'] = 'localhost'
-
+default['base']['bk_dbnames'] = ["wp", "phpbb", "mysql" ]
 case node[:platform]
 when 'centos', 'fedora'
   default['base']['sysconfig'] = '/etc/sysconfig'
--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -6,3 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+template '/usr/local/1mg/bin/backup.sh' do
+  source 'backup.sh.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+cron 'backup' do
+  hour '0'
+  minute '0'
+  command 'sh /usr/local/1mg/bin/backup.sh'
+end
