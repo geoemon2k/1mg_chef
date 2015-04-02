a6586dd38387bf5308ffb495c35fa9154843f946
uehata_new <uehata2@firstserver.co.jp>
2015-04-02 20:45:39 +0900

    remove sendmail

---
+++ cookbooks/sendmail/recipes/unsetup.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: sendmail
+# Recipe:: unsetup
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package 'sendmail' do
+  action :remove
+end
