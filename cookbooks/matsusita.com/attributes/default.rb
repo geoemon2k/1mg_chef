c195899f9b960b161a86049552373a5ca9cc6f91
uehata_new <uehata2@firstserver.co.jp>
2015-07-06 14:57:10 +0900

    add cookbooks/matsusita

---
+++ cookbooks/matsusita.com/attributes/default.rb
@@ -0,0 +1,4 @@
+default['matsusita']['owner'] = 'matsusita'
+default['matsusita']['group'] = 'matsusita'
+ 
+default['matsusita']['docroot'] = '/home/matsusita.com'
---
+++ cookbooks/matsusita.com/recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: matsusita.com
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+group node['matsusita']['group'] do
+  action :create
+  gid 998
+  members ['paco','vicente']
+end
+
+user node['matsusita']['user'] do
+  action :create
+  uid 998
+  gid node['matsusita']['group']
+  home node['matsusita']['docroot']
+  shell '/bin/bash'
+  supports :manage_home => true, :non_unique => false
+end
+
+directory node['matsusita']['docroot'] do
+  owner node['matsusita']['owner']
+  group node['matsusita']['group']
+  mode "0755"
+  action :create
+end
