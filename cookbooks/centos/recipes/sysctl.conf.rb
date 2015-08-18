4f3dde4fcfb8d5cf63867a31a0a0a144a542a5f3
uehata_new <uehata2@firstserver.co.jp>
2015-08-18 11:47:00 +0900

    fixed sysctl

--- cookbooks/centos/recipes/sysctl.conf.rb
+++ cookbooks/centos/recipes/sysctl.conf.rb
@@ -1,10 +1,23 @@
 if node['centos']['sysctl.conf'] != nil
-  node['centos']['sysctl.conf'].each do |params|
-    bash "change sysctl.conf_#{params['name']}" do
-      code <<-EOH
-        sed -i -e "s/#{params['name']} =.*/#{params['name']} = #{params['value']}/g" /etc/sysctl.conf
-        sysctl #{params['name']}=#{params['value']}
-      EOH
+  if node['platform'] == 'centos'
+    template '/etc/sysctl.conf' do
+      source 'sysctl.conf.cent' + node['platform_version'].to_i + '.erb'
+      owner 'root'
+      group 'root'
+      mode '0644'
+      variables {
+        sysctl => node['centos']['sysctl.conf']
+      }
     end
   end
+
+  node['centos']['sysctl.conf'].each do |sysctl_params|
+    if sysctl_params['name'].nil? == false && sysctl_params['value'].nil? == false
+      execute 'exec sysctl' do
+        command "sysctl #{sysctl_params['name']}=#{sysctl_params['value']}"
+        creates '/tmp/something'
+        action :run
+      end
+    end
+  end  
 end
