8d955c7875ae53319cac9ea13ec73ec1a986d78d
uehata_new <uehata2@firstserver.co.jp>
2016-10-27 16:41:04 +0900

    fixed base/attributes

--- cookbooks/base/attributes/default.rb
+++ cookbooks/base/attributes/default.rb
@@ -10,8 +10,6 @@ when 'centos', 'fedora'
   default['base']['off_services'] = Array.new
   default['base']['on_services'] = Array.new
 end
-default['base']['packages'] = [
-{
-  name => 'zsh'
-}
-]
+default['base']['packages'] = [{
+  'name' => 'zsh'
+}]
