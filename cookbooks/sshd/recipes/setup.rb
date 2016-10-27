a82a0bf549fd68c01b1dae06099462b087fd3a55
uehata_new <uehata2@firstserver.co.jp>
2016-10-27 17:37:06 +0900

    fixed sshd

--- cookbooks/sshd/recipes/setup.rb
+++ cookbooks/sshd/recipes/setup.rb
@@ -28,34 +28,35 @@ group 'sftponly' do
   gid 1001
 end
 
+if node['sshd']['sftp_users'].nil? == false && node['sshd']['sftp_users'].empty? == false
+  node["sshd"]["sftp_users"].each do |sftp_users|
+    link "/home/#{sftp_users['user']}" do
+      to "/usr/local/1mg/data/#{sftp_users['user']}"
+    end
 
-node["sshd"]["sftp_users"].each do |sftp_users|
-  link "/home/#{sftp_users['user']}" do
-    to "/usr/local/1mg/data/#{sftp_users['user']}"
-  end
+    user sftp_users["user"] do
+      uid sftp_users["uid"]
+      action :create
+      gid "sftponly"
+      shell '/bin/zsh'
+      home "/home/#{sftp_users['user']}"
+    end
 
-  user sftp_users["user"] do
-    uid sftp_users["uid"]
-    action :create
-    gid "sftponly"
-    shell '/bin/zsh'
-    home "/home/#{sftp_users['user']}"
-  end
-
-  directory "/home/#{sftp_users['user']}/.ssh/" do
-    owner sftp_users['user']
-    group 'sftponly'
-    mode '0755'
-    action :create
-  end
-  
-  template "/home/#{sftp_users['user']}/.ssh/authorized_keys" do
-    source 'authorized_keys.erb'
-    owner sftp_users['user']
-    group 'sftponly'
-    mode '0400'
-    variables({
-      :authkey => sftp_users['authkey']
-    })
+    directory "/home/#{sftp_users['user']}/.ssh/" do
+      owner sftp_users['user']
+      group 'sftponly'
+      mode '0755'
+      action :create
+    end
+    
+    template "/home/#{sftp_users['user']}/.ssh/authorized_keys" do
+      source 'authorized_keys.erb'
+      owner sftp_users['user']
+      group 'sftponly'
+      mode '0400'
+      variables({
+        :authkey => sftp_users['authkey']
+      })
+    end
   end
 end
